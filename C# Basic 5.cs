06c8074e82989eeb71f9996666ec95668125d23f
Gustavo Lora <132119031+Gustavo-045@users.noreply.github.com>
2024-08-25 21:52:39 -0500

    Add files via upload

---
+++ C# Basic 5.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Inundaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Programa para evaluar inundaciones en Antioquia");
+            Console.WriteLine("hay 3 tipos de inundaciones (1) Fluviales, (2) Costeras, (3) urbanas");
+
+            int totalinudaciones = 0;
+            int tipoinundacion;
+            int hogaresafectados = 0;
+
+            // cantidadhogaresafectadosportipo es el total carga y la cantidad tipo de inundaciones es el total de lanzamientos
+
+            int[] cantidadtipoinundaciones = new int[3 + 1];
+
+            float[] cantidadhogaresafectadosportipo = new float[3 + 1];
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                cantidadhogaresafectadosportipo[i] = 0;
+                cantidadtipoinundaciones[i] = 0;
+            }
+            while (totalinudaciones < 12)
+            {
+                try
+                {
+                    Console.WriteLine($"ingresa el tipo de inundacion para el caso numero {totalinudaciones + 1}, (1) Fluviales, (2) Costeras, (3) urbanas ", totalinudaciones + 1);
+                    tipoinundacion = int.Parse(Console.ReadLine()!);
+
+                    if (tipoinundacion == 1 || tipoinundacion == 2 || tipoinundacion == 3)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Ingresa la cantidad de hogares afectados");
+                            hogaresafectados = int.Parse(Console.ReadLine()!);
+
+                            if (hogaresafectados >= 0 && hogaresafectados <= 100)
+                            {
+                                switch (tipoinundacion)
+                                {
+                                    case 1:
+                                        cantidadtipoinundaciones[1]++;
+                                        cantidadhogaresafectadosportipo[1] += hogaresafectados;
+                                        break;
+
+                                    case 2:
+                                        cantidadtipoinundaciones[2]++;
+                                        cantidadhogaresafectadosportipo[2] += hogaresafectados;
+                                        break;
+
+                                    case 3:
+                                        cantidadtipoinundaciones[3]++;
+                                        cantidadhogaresafectadosportipo[3] += hogaresafectados;
+                                        break;
+
+
+
+                                }
+                                totalinudaciones++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("ingresaste un cantidad de hogares fuera del rango 0 a 100, intenta nuevamente ");
+                            }
+                        }
+                        catch (FormatException error)
+                        {
+                            Console.WriteLine("ingresaste un dato no numerico, intenta nuevamente");
+                            Console.WriteLine(error.Message);
+                        }
+
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("ingresaste un tipo de inundacion invalida, intentalo de nuevo");
+                    }
+
+                }
+
+                catch (FormatException error)
+                {
+                    Console.WriteLine("ingresaste un dato no numerico, intenta nuevamente");
+                    Console.WriteLine(error.Message);
+                }
+
+            }
+
+            // calcularemos el promedio
+            float[] promedios = CalculaPromedioAfectacion(cantidadtipoinundaciones, cantidadhogaresafectadosportipo);
+
+            //visualizamos los resultados aqui
+            Console.WriteLine("\n\nresultados obtenidos para inundaciones: \n");
+
+            Console.Write("\nTipo de Inundacion: \tFluvial \tCostera \tUrbana");
+            Console.Write("\ncantidad:                        \t");
+            for (int i = 0; i < 3; i++)
+                Console.Write(cantidadtipoinundaciones[i + 1] + "\t");
+
+            Console.Write("\nhogares afectados:                \t");
+            for (int i = 0; i < 3; i++)
+                Console.Write(cantidadhogaresafectadosportipo[i + 1] + "\t");
+
+
+            Console.Write("\npromedio:                          \t");
+            for (int i = 0; i < 3; i++)
+                Console.Write(promedios[i + 1] + "\t");
+
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        static float[] CalculaPromedioAfectacion(int[] arregloInundaciones, float[] arregloCasas)
+        {
+            float[] promedios = new float[3 + 1];
+
+            for (int i = 0; i < promedios.Length; i++)
+            {
+                if (arregloInundaciones[i] == 0)
+                    promedios[i] = 0;
+                else
+                    promedios[i] = arregloCasas[i] / arregloInundaciones[i];
+            }
+
+            return promedios;
+        }
+    }
+}
---
+++ C# Basic7.cs
@@ -0,0 +1,168 @@
+﻿
+
+namespace FEDRA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al programa de proteccion ambiental de FEDRA");
+
+            //variables
+            float cantidadhogares, roedores = 0, hongos = 0, insectos = 0, nofumigados = 0, hogaresfumigados = 0;
+            bool datocorrecto;
+
+            datocorrecto = false;
+
+
+
+            while (datocorrecto == false)
+            {
+                try
+                {
+                    Console.Write("\ndigite la cantida de casas que se fumigaron con enfasis en roedores  ");
+                    roedores = float.Parse(Console.ReadLine()!);
+
+                    if (roedores >= 0)
+                        datocorrecto = true;
+                    else
+                        Console.WriteLine("el numero de casas debe ser mayor a cero");
+                }
+                catch (FormatException error)
+                {
+                    Console.WriteLine("Ingresaste un dato no numérico. Intenta nuevamente!");
+                    Console.WriteLine($"Error: {error.Message}\n");
+                }
+
+                hogaresfumigados = hogaresfumigados + roedores;
+            }
+
+
+
+
+            datocorrecto = false;
+
+            while (datocorrecto == false)
+            {
+                try
+                {
+                    Console.Write("\ndigite la cantida de casas que se fumigaron con enfasis en hongos  ");
+                    hongos = float.Parse(Console.ReadLine()!);
+
+                    if (hongos >= 0)
+                        datocorrecto = true;
+                    else
+                        Console.WriteLine("el numero de casas debe ser mayor a cero");
+                }
+                catch (FormatException error)
+                {
+                    Console.WriteLine("Ingresaste un dato no numérico. Intenta nuevamente!");
+                    Console.WriteLine($"Error: {error.Message}\n");
+                }
+
+                hogaresfumigados = hogaresfumigados + hongos;
+            }
+
+
+            datocorrecto = false;
+            while (datocorrecto == false) 
+            {
+                try
+                {
+                    Console.Write("\ndigite la cantida de casas que se fumigaron con enfasis en insectos  ");
+                    insectos = float.Parse(Console.ReadLine()!);
+
+                    if (insectos >= 0)
+                        datocorrecto = true;
+                    else
+                        Console.WriteLine("el numero de casas debe ser mayor a cero");
+                }
+                catch (FormatException error)
+                {
+                    Console.WriteLine("Ingresaste un dato no numérico. Intenta nuevamente!");
+                    Console.WriteLine($"Error: {error.Message}\n");
+                }
+
+                hogaresfumigados = hogaresfumigados + insectos;
+            }
+
+
+            datocorrecto = false;
+            while (datocorrecto == false) 
+            {
+                try
+                {
+                    Console.Write("\ndigite la cantida de casas que se no fueron fumigadas  ");
+                    nofumigados = float.Parse(Console.ReadLine()!);
+
+                    if (nofumigados >= 0)
+                        datocorrecto = true;
+                    else
+                        Console.WriteLine("el numero de casas debe ser mayor a cero");
+                }
+                catch (FormatException error)
+                {
+                    Console.WriteLine("Ingresaste un dato no numérico. Intenta nuevamente!");
+                    Console.WriteLine($"Error: {error.Message}\n");
+                }
+
+                
+            }
+
+
+
+            cantidadhogares = (nofumigados + hogaresfumigados);
+            float porcroedores = (roedores / cantidadhogares) * 100;
+            float porchongos = (hongos / cantidadhogares) * 100;
+            float porcinsectos = (insectos / cantidadhogares) * 100;
+            float porcno = (nofumigados / cantidadhogares) * 100;
+
+
+            Console.WriteLine("\nCalculando...");
+
+
+            Console.WriteLine($"\nCantidad de casas de la residencia: {cantidadhogares}");
+
+            Console.WriteLine($"Cantidad de casas fumigadas: {hogaresfumigados }");
+
+            Console.WriteLine($"\nCantidad de casas no fumigadas: {nofumigados}");
+            Console.WriteLine($"porcentaje de casas no fumigadas: {porcno.ToString("00.00")}%");
+
+            Console.WriteLine($"\ntotal de casas fumigadas con enfasis en roedores: {roedores}");
+            Console.WriteLine($"porcentaje de casas fumigadas con enfasis en roedores: {porcroedores.ToString("00.00")}%");
+            
+            Console.WriteLine($"\ntotal de casas fumigadas con enfasis en hongos: {hongos}");
+            Console.WriteLine($"porcentaje de casas fumigadas con enfasis en hongos: {porchongos.ToString("00.00")}%");
+            
+            Console.WriteLine($"\ntotal de casas fumigadas con enfasis en insectos: {insectos}");
+            Console.WriteLine($"porcentaje de casas fumigadas con enfasis en insectos: {porcinsectos.ToString("00.00")}%");
+
+            if (roedores > hongos && roedores > insectos)
+                Console.WriteLine("\nla cantidad de casas fumigadas con enfasis en roderoes fue mayor a la cantidad de casas fumigadas para otras plagas");
+
+
+            if (hongos > roedores && hongos > insectos)
+                Console.WriteLine("\nla cantidad de casas fumigadas con enfasis en hongos fue mayor a la cantidad de casas fumigadas para otras plagas");
+
+
+            if (insectos > hongos && insectos > roedores)
+                Console.WriteLine("\nla cantidad de casas fumigadas con enfasis en insectos fue mayor a la cantidad de casas fumigadas para otras plagas");
+
+
+            if (hogaresfumigados == cantidadhogares) 
+            Console.WriteLine("\nFumigacion Exitosa");
+            else
+            Console.WriteLine("\nFumigacion Incompleta");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
